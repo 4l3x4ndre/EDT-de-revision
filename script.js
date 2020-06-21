9a6929b34a9789112e01cd5dedccfe46cc994ff2
4l3x4ndre <alexandre@amrani.fr>
2020-06-21 13:36:23 +0200

    reglage des mois et des jours

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 let jours_a_remonter = [0, 3, 5, 8, 13, 21]
-let jours_noms = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-let jours_noms_FR = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche']
+let jours_noms = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+let jours_noms_FR = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi']
 
 let emploi_du_temps = [
+    {'Dimanche': []},
     {'Lundi': ['histoire', 'français']},
     {'Mardi': ['maths']},
     {'Mercredi': ['histoire']},
     {'Jeudi': ['maths', 'français']},
     {'Vendredi': ['histoire', 'maths', 'français']},
     {'Samedi': []},
-    {'Dimanche': []}
 ]
 let aujourdhui = new Date()
-afficherSurLaPage(jours_noms_FR[aujourdhui.getDay()] + " " + aujourdhui.getDate().toString() + "/" + aujourdhui.getMonth(), 'div', 'h1')
+afficherSurLaPage(jours_noms_FR[aujourdhui.getDay()] + " " + aujourdhui.getDate().toString() + "/" + (parseInt(aujourdhui.getMonth())+1), 'div', 'h1')
 for (let i = 0; i < jours_a_remonter.length; i++) {
     const nb = jours_a_remonter[i];
     let jour_a_reviser = new Date(aujourdhui - nb*3600*24*1000)
 
-    let index_du_jour_a_reviser = jour_a_reviser.getDay()-1
+    let index_du_jour_a_reviser = jour_a_reviser.getDay()
     index_du_jour_a_reviser < 0 ? index_du_jour_a_reviser = 0 : null
     let nom_du_jour = jours_noms_FR[index_du_jour_a_reviser]
-    console.log(jour_a_reviser.getDay())
+    console.log(jour_a_reviser.getDay(), aujourdhui, jour_a_reviser,aujourdhui- nb*3600*24*1000)
 
     let matieres = emploi_du_temps[index_du_jour_a_reviser][nom_du_jour]
     if (matieres.length == 0)
@@ -32,7 +32,7 @@ for (let i = 0; i < jours_a_remonter.length; i++) {
         matieres_txt += matiere + " "
     }
 
-    afficherSurLaPage(nom_du_jour+ " " + jour_a_reviser.getDate().toString() + "/" + jour_a_reviser.getMonth(), 'div', 'div1')
+    afficherSurLaPage(nom_du_jour+ " " + jour_a_reviser.getDate().toString() + "/" + (parseInt(jour_a_reviser.getMonth())+1), 'div', 'div1')
     afficherSurLaPage(matieres_txt, 'div', 'div1')
     afficherSurLaPage("", 'div', 'div1')
 }
